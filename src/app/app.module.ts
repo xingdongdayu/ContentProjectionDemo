2c1d93b175da363584074acc15e6f00196cdef54
Dong <xingdongdayu@users.noreply.github.com>
2017-08-07 17:26:43 -0700

    create component child2 for ngOnDestroy demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ChildComponent } from './child/child.component';
+import { Child2Component } from './child2/child2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChildComponent
+    ChildComponent,
+    Child2Component
   ],
   imports: [
     BrowserModule
